1800598b5da314be450d0c1436fc7513ae92b049
rulev <rulev.vania@gmail.com>
2024-05-22 19:47:56 +0300

    complete ui-kit

--- src/app/modules/ui-kit/components/ui-kit-main-page/ui-kit-main-page.component.ts
+++ src/app/modules/ui-kit/components/ui-kit-main-page/ui-kit-main-page.component.ts
@@ -1,10 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {MatCheckboxModule} from "@angular/material/checkbox";
 
 @Component({
   selector: 'app-ui-kit-main-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule, NgOptimizedImage, MatCheckboxModule],
   templateUrl: './ui-kit-main-page.component.html',
   styleUrls: ['./ui-kit-main-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
